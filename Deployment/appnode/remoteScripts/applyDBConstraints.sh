af3ddb83cf3150e2cb7b204239ef4cffad909a03
Jake Callery <jcallery@subvoicestudios.com>
2017-10-02 11:02:56 -0400

    working through ansible playbooks and scripts lxc creation works

--- Deployment/appnode/remoteScripts/applyDBConstraints.sh
+++ Deployment/appnode/remoteScripts/applyDBConstraints.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-node /cheevos/DBTools/dbtools.js --apply-constraints
+#node /cheevos/DBTools/dbtools.js --apply-constraints
 touch /etc/neo4j/db_constraints_applied
--- Deployment/appnode/remoteScripts/buildClient.sh
+++ Deployment/appnode/remoteScripts/buildClient.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /cheevos/webclient/src
+cd /openvote/Client/src
 webpack
